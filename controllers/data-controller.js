7b722b1abf5fa6b2df4401c9edbd3be055d03d46
Akshat Arora <akshatarora826@gmail.com>
2017-08-17 23:33:29 +0530

    set the token expiry
    the user can't add the data to the database once his/her token has expired.

--- controllers/data-controller.js
+++ controllers/data-controller.js
@@ -1,4 +1,5 @@
 var Person = require('../models/person.js');
+var jwt = require('jsonwebtoken');
 
 module.exports.getData = function(req,res){
 	Person.find({},function(err,buddy){
@@ -11,10 +12,21 @@ module.exports.getData = function(req,res){
 
 module.exports.postData = function(req,res){
 	var person = new Person(req.body);
+jwt.verify(req.body.token, process.env.SECRET_KEY, function(err, decoded) {
+		    console.log(err, decoded);
+		    if(decoded)
+		    	{
+		    		res.send("your token has expired")}
+else{
 	person.save(function(err){
 		if(err){
 			return res.status(500).send("couldn't save the user at this time.")
 		}
+});
+
+
+		
+
 		res.status(200).send("you have added a new person")
-	})
+}});
 }
